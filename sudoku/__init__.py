0282bfd71eecc7803ece7dadc0a2a66601dbf3db
Chirurgus <sasha.sashaxl@gmail.com>
2020-01-09 22:19:21 +0100

    Import exported names to sudoku.__init__

--- sudoku/__init__.py
+++ sudoku/__init__.py
@@ -1,4 +1,7 @@
 # Created by Oleksandr Sorochynskyi, and Sebastien Meyer
 # On 09/01/2020
 
+from .sgrid import SudokuGrid
+from .solve import solve_sudoku
+
 __all__ = ['SudokuGrid', 'solve_sudoku',]
