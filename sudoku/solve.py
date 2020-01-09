ca8a4a19ef61f78b884f30d2c54256c447bfe15c
MEYER Sébastien <35813232+Shiaroku@users.noreply.github.com>
2020-01-09 23:51:49 +0100

    Update solve.py
    Renaming here too

--- sudoku/solve.py
+++ sudoku/solve.py
@@ -27,7 +27,7 @@ def _solve_recursive(sudoku):
         ## Better way (from 12 to 2.5 seconds in average for hardest sudoku):
         ## possible_values=[sudoku.possible_values(c) for c in cells]
         ## With this further optimisation by grouping reusable computation we get almost 10 times faster
-        possible_values=sudoku.opt_possible_values_for_multiple_cells(cells)
+        possible_values=sudoku.possible_values_vectorized(cells)
         npv=[len(tpl) for tpl in possible_values]
         cell=cells[npv.index(min(npv))]
     except Exception:
